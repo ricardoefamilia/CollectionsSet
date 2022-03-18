fe1e636f2737dc30e906d2c457934792044ceecd
ricardoefamilia <ricardoefamilia@gmail.com>
2022-03-18 10:27:46 -0300

    ExemploOrdenacaoSet

---
+++ src/Serie.java
@@ -0,0 +1,59 @@
+import java.util.Objects;
+
+public class Serie {
+    private String nome;
+    private String genero;
+    private Integer tempoEpisodio;
+
+    public Serie(String nome, String genero, Integer tempoEpisodio) {
+        this.nome = nome;
+        this.genero = genero;
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    @Override
+    public String toString() {
+        return "Serie{" +
+                "nome='" + nome + '\'' +
+                ", genero='" + genero + '\'' +
+                ", tempoEpisodio=" + tempoEpisodio +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Integer getTempoEpisodio() {
+        return tempoEpisodio;
+    }
+
+    public void setTempoEpisodio(Integer tempoEpisodio) {
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Serie serie = (Serie) o;
+        return Objects.equals(nome, serie.nome) && Objects.equals(genero, serie.genero) && Objects.equals(tempoEpisodio, serie.tempoEpisodio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, genero, tempoEpisodio);
+    }
+}
