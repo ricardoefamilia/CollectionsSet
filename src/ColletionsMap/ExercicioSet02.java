f34c663d134d30771e89a4f5d5b8207efb424932
ricardo <ricardoefamilia@gmail.com>
2022-03-26 19:14:07 -0300

    ColletionsMap Exercícios dados 100 vezes - ajustado

--- src/ColletionsMap/ExercicioSet02.java
+++ src/ColletionsMap/ExercicioSet02.java
@@ -13,18 +13,19 @@ public class ExercicioSet02 {
             put(5,0);
             put(6,0);
         }};
-        int num=0, x1=0, x2=0, x3=0, x4=0, x5=0, x6=0;
-        for(int x=0; x < 100; x++){
+        int num=0, x, x1=0, x2=0, x3=0, x4=0, x5=0, x6=0;
+        for(x=0; x < 100; x++){
             num = (int) Math.ceil(Math.random() * 6);
             switch (num){
-                case 1: dados.put(1,(x1++));break;
-                case 2: dados.put(2,(x2++));break;
-                case 3: dados.put(3,(x3++));break;
-                case 4: dados.put(4,(x4++));break;
-                case 5: dados.put(5,(x5++));break;
-                case 6: dados.put(6,(x6++));break;
+                case 1: dados.put(1,(++x1));break;
+                case 2: dados.put(2,(++x2));break;
+                case 3: dados.put(3,(++x3));break;
+                case 4: dados.put(4,(++x4));break;
+                case 5: dados.put(5,(++x5));break;
+                case 6: dados.put(6,(++x6));break;
             }
         }
         System.out.println(dados);
+        System.out.println(x);
     }
 }
