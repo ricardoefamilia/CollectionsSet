b6583c698d1819ef58c6297b4d8675f6a59c0d9c
ricardo <ricardoefamilia@gmail.com>
2022-03-31 22:45:41 -0300

    ColletionsMap Exercício Agenda de contatos ok

--- src/ColletionsMap/ExercAgenda.java
+++ src/ColletionsMap/ExercAgenda.java
@@ -1,15 +1,13 @@
 package ColletionsMap;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 /*
 Crie uma agenda exibindo id, nome, contato
  */
 public class ExercAgenda {
     public static void main(String[] args) {
-        System.out.println("*** \tOrdem Aleatória\t ***");
+        System.out.println("***\tOrdem Aleatória\t***");
         Map<Integer, Agenda> minhaAgenda = new HashMap<>(){{
            put(1,new Agenda("Rose", "61 35555-7777"));
             put(4,new Agenda("Moisés", "61 95555-7666"));
@@ -20,8 +18,9 @@ public class ExercAgenda {
         for (Map.Entry<Integer, Agenda> agenda: minhaAgenda.entrySet()) {
             System.out.println(agenda.getKey()+": "+ agenda.getValue().getNome()+" - "+agenda.getValue().getContato());
         }
+        System.out.println();
 
-        System.out.println("*** \tOrdem de Entrada\t ***");
+        System.out.println("***\tOrdem de Entrada\t***");
         Map<Integer, Agenda> minhaAgenda1 = new LinkedHashMap<>(){{
             put(1,new Agenda("Rose", "61 35555-7777"));
             put(4,new Agenda("Moisés", "61 95555-7666"));
@@ -30,6 +29,23 @@ public class ExercAgenda {
         }};
 
         for (Map.Entry<Integer, Agenda> agenda: minhaAgenda1.entrySet()) {
+            System.out.println(agenda.getKey()+": "+ agenda.getValue().getNome()+" - "+agenda.getValue().getContato());
+        }
+        System.out.println();
+
+        System.out.println("***\tOrdem por Id\t***");
+        Map<Integer, Agenda> minhaAgenda2 = new TreeMap<>(minhaAgenda);
+
+        for(Map.Entry<Integer, Agenda> agenda : minhaAgenda2.entrySet()){
+            System.out.println(agenda.getKey()+": "+ agenda.getValue().getNome()+" - "+agenda.getValue().getContato());
+        }
+        System.out.println();
+
+        System.out.println("***\tOrdem por Nome\t***");
+        Set<Map.Entry<Integer, Agenda>> minhaAgenda3 = new TreeSet<>(new ComparatorContatoNome());
+        minhaAgenda3.addAll(minhaAgenda.entrySet());
+
+        for(Map.Entry<Integer, Agenda> agenda : minhaAgenda3){
             System.out.println(agenda.getKey()+": "+ agenda.getValue().getNome()+" - "+agenda.getValue().getContato());
         }
     }
@@ -56,5 +72,13 @@ class Agenda{
 
     public String getContato() {
         return contato;
+    }
+}
+
+class ComparatorContatoNome implements Comparator<Map.Entry<Integer, Agenda>>{
+
+    @Override
+    public int compare(Map.Entry<Integer, Agenda> a1, Map.Entry<Integer, Agenda> a2) {
+        return a1.getValue().getNome().compareToIgnoreCase(a2.getValue().getNome());
     }
 }
